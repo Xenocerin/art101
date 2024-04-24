3a8ac12b914ea8799375824015e0bf3d83ea445c
Xenocerin <francoeur.stephen@gmail.com>
2024-04-24 04:20:12 -0700

    lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,29 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Battleship
+//An implimentation of the game
+// Author: Stephen Francoeur
+// Date:  4/24/2024
+
+//Take user boat locations
+    //Starting with creating the largest ship, represented by Os, at center
+        //Move location with arrow keys, change orientation with “r”
+            //If any segment of ship would be off grid deny move
+    //“Enter” to confirm then repeat until smallest ship
+//Randomize computer boat locations]
+		//Randomly place ships in descending order from largest to smallest
+        //If any segment of ship would be off grid place again
+//Print board after inputs
+    //Print a row of ten Xs, Ms if misses, Hs if hits indent at end
+        //Repeat until ten rows
+//Accept user inputs
+    //Take user input in form of a letter and number i.e. “D1”                                             
+		//Place M or H on corresponding tile depending on if miss or hit
+//Choose computer inputs
+    //Place hit marker in center of spaces with width and height equal to largest      surviving enemy ship
+			  //If no spaces meet dimensions decrease area by one width and one height but not less than 2                                           
+//Test if win condition
+	  //If player var shipsRemaining = 0 computer win
+		    //If computer var shipsRemaining = 0 player win
+
 
 // Constants
 
@@ -19,3 +42,5 @@ function main() {
 
 // let's get this party started
 main();
+
+
