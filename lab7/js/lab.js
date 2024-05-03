9199d6be2fd70863a08ba6c0c09cf87a484f08ec
Xenocerin <francoeur.stephen@gmail.com>
2024-05-03 00:53:04 -0700

    73

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -1,17 +1,22 @@
 // Lab 7 - Arrays & Objects
 // Author: Stephen Francoeur
 // Date: 5/2/2024
+function combinedCaseSort(a,b) {
+  return a.toLowerCase().localeCompare(b.toLowerCase());
+}
+
 function sortUserName() {
   var userName = window.prompt("Hello, please input your username for correction");
   console.log("userName =", userName);
   var nameArray = userName.split('');
   console.log("nameArraySort =", nameArray);
-  var nameArraySort = nameArray.sort();
+  var nameArraySort = nameArray.sort(combinedCaseSort);
   console.log("nameArraySort =", nameArraySort);
   var nameSorted = nameArraySort.join('');
   console.log("nameSorted =", nameSorted);
   return nameSorted;
 }
+
 
 document.writeln("Oh hey, I've fixed your name: ",
    sortUserName(), "</br>");
