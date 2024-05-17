9572b3c373ab7dfaabebf3e56b4d85a00faf6b66
Xenocerin <francoeur.stephen@gmail.com>
2024-05-17 00:27:05 -0700

    10

--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -2,20 +2,10 @@
 // Author: Stephen Francoeur
 // Date: 5/13/2024
 
-$("#challenge").append("<button id='button-challenge'>Specialize</button>")
+$(".challenge").append("<button id='button-challenge'>Specialize</button>")
 
 $("button-challenge").click(function() {
-  $(this).parent().toggleClass("special");
+$(".challenge").toggleClass(".special");
 });
 
-$("#problems").append("<button id='button-problems'>Specialize</button>")
 
-$("button-problems").click(function() {
-  $(this).parent().toggleClass("special");
-});
-
-$("#reflection").append("<button id='button-reflect'>Specialize</button>")
-
-$("button-reflect").click(function() {
-  $(this).parent().toggleClass("special");
-});
