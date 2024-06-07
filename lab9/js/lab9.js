6b203d5555c1ea8cd0be1dbab7dc8a787d23af6a
Xenocerin <francoeur.stephen@gmail.com>
2024-06-06 19:44:35 -0700

    14-2

--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -2,11 +2,14 @@
 // Author: Stephen Francoeur
 // Date: 5/13/2024
 
+/*appends a button to each div with the class "challenge", 
+that button will appear saying "make special" 
+and will have the class "button challenge".*/
 $(".challenge").append("<button class='button-challenge'>Make Special</button>");
 
-
+/*upon clicking any button with the class "button challenge" will toggle on the css class 
+"special" for the div the button is appended to through an anonymous function.*/
 $(".button-challenge").click(function(){
-  $(this).parent().toggleClass("minor-section");
   $(this).parent().toggleClass("special");  
 })
 
